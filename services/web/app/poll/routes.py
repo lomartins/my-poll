ef5524b52ddbd5b2cf6e696f3461cb18ce02b282
Lucas Martins <lucasmariano.martins@outlook.com>
2021-07-23 10:24:30 -0300

    :sparkles: Add create poll and poll result backend

--- services/web/app/poll/routes.py
+++ services/web/app/poll/routes.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user
 
-from .forms import get_poll_form
+from .forms import get_poll_form, CreatePollForm
 from .. import db
 from ..models import Poll, Choice, Vote
 
@@ -37,3 +39,28 @@ def poll_page(poll_id):
         return redirect(url_for('Polls.poll_page', poll_id=poll_id))
 
     return render_template('poll/poll.html', poll_id=poll_id, poll=poll, form=form)
+
+
+@blueprint.route('/<int:poll_id>/result', methods=['GET', 'POST'])
+def poll_result(poll_id):
+    poll = Poll.query.get(poll_id)
+    choices = Choice.query.filter_by(poll_id=poll_id).order_by(Choice.id.asc()).all()
+
+    return render_template('poll/poll_result.html', poll=poll, choices=choices)
+
+
+@blueprint.route('/create', methods=['GET', 'POST'])
+def create_poll():
+    form = CreatePollForm(request.form)
+
+    if form.validate_on_submit():
+        poll = Poll(
+            question=form.question.data,
+            pub_date=datetime.utcnow()
+        )
+        db.session.add(poll)
+        db.session.commit()
+
+        return "Redirect to poll control panel page"  # TODO
+
+    return render_template('poll/create-poll.html', form=form)
